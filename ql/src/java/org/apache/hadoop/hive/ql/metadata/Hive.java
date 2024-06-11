1fbedd17f596181b5343ea511acbe379b4edaad3
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2024-06-11 10:56:14 +0200

    Fix UGI comparison

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
@@ -446,7 +446,8 @@ public class Hive {
 
   private boolean isCurrentUserOwner() throws HiveException {
     try {
-      return owner == null || owner.toString().equals(UserGroupInformation.getCurrentUser().toString());
+      LOG.info("Using equals2 for comparison.");
+      return owner == null || owner.equals2(UserGroupInformation.getCurrentUser());
     } catch(IOException e) {
       throw new HiveException("Error getting current user: " + e.getMessage(), e);
     }
